212fa566ffbb589c41b44853ddfccd8c46b218e1
ice <hoticealemk254@gmail.com>
2025-01-22 14:51:42 -0800

    Initialize commi

---
+++ wecome_page.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const dropdownMenu = document.querySelector('.dropdown_menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
